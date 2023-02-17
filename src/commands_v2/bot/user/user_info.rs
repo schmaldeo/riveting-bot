937962e0854da74360243a50aabfb0f2e223586c
Samzyre <samihtk@outlook.com>
2023-02-17 04:27:59 +0200

    Add more userinfo

--- src/commands_v2/bot/user/user_info.rs
+++ src/commands_v2/bot/user/user_info.rs
@@ -1,8 +1,10 @@
-use twilight_util::builder::embed::{self, ImageSource};
+use twilight_mention::Mention;
+use twilight_util::builder::embed::{self, EmbedFieldBuilder, ImageSource};
 
-use crate::commands_v2::arg::Ref;
 use crate::commands_v2::prelude::*;
 use crate::utils::prelude::*;
+
+// Useful: https://discord.com/developers/docs/reference#image-formatting-cdn-endpoints
 
 /// Command: Get information about user.
 pub struct UserInfo {
@@ -18,45 +20,66 @@ impl UserInfo {
             .option(user("user", "Mention a user"))
     }
 
-    pub async fn classic(_ctx: Context, _req: ClassicRequest) -> CommandResult {
+    async fn classic(_ctx: Context, _req: ClassicRequest) -> CommandResult {
         todo!();
     }
 
-    pub async fn slash(ctx: Context, req: SlashRequest) -> CommandResult {
+    async fn slash(ctx: Context, req: SlashRequest) -> CommandResult {
+        let Some(guild_id) = req.interaction.guild_id else {
+            return Err(CommandError::Disabled);
+        };
+
         // If no args provided, check own props
         let user_id = match req.args.user("user") {
-            Ok(Ref::Id(user_to_get)) => user_to_get,
-            _ => req.interaction.author_id().unwrap(),
+            Ok(user) => user.id(),
+            _ => req
+                .interaction
+                .author_id()
+                .ok_or(CommandError::MissingArgs)?,
         };
 
-        let request = ctx.http.user(user_id).send().await?;
-        let username = request.name;
-
-        let embed = embed::EmbedBuilder::new()
-            .title(username)
-            .color(request.accent_color.unwrap_or(0));
-
-        // TODO get role names from these markers
-        // let roles = ctx
-        //     .http
-        //     .guild_member(req.interaction.guild_id.unwrap(), user_id)
-        //     .send()
-        //     .await
-        //     .unwrap()
-        //     .roles;
-
-        // println!("{:?}", roles);
+        let member = ctx.http.guild_member(guild_id, user_id).send().await?;
+        // Somewhat of a redundant call, but some data may be missing on `member.user`.
+        let user = ctx.http.user(user_id).send().await?;
 
         // If no avatar for the user, get the default one
-        let url = match request.avatar {
+        let image_url = match member.avatar.or(user.avatar) {
             Some(avatar) => {
-                format!("https://cdn.discordapp.com/avatars/{user_id}/{avatar}.png?size=2048")
+                format!("https://cdn.discordapp.com/avatars/{user_id}/{avatar}.png?size=4096")
             },
-            _ => "https://cdn.discordapp.com/embed/avatars/0.png".to_string(),
+            _ => {
+                let discriminator = user.discriminator % 5;
+                format!("https://cdn.discordapp.com/embed/avatars/{discriminator}.png")
+            },
+            // _ => "https://cdn.discordapp.com/embed/avatars/0.png".to_string(),
         };
 
-        let image_source = ImageSource::url(url)?;
-        let embed = embed.image(image_source).build();
+        let mut embed = embed::EmbedBuilder::new();
+
+        if let Some(banner) = user.banner {
+            embed = embed.thumbnail(ImageSource::url(format!(
+                "https://cdn.discordapp.com/banners/{user_id}/{banner}.png?size=4096"
+            ))?);
+        }
+
+        if let Some(nick) = member.nick {
+            embed = embed.field(EmbedFieldBuilder::new("AKA", nick).inline());
+        }
+
+        let roles: String = member
+            .roles
+            .into_iter()
+            .map(|i| format!("{} ", i.mention()))
+            .collect();
+        let roles = roles.trim();
+        let roles = if roles.is_empty() { "-" } else { roles };
+
+        let embed = embed
+            .title(user.name)
+            .color(user.accent_color.unwrap_or(0))
+            .image(ImageSource::url(image_url)?)
+            .field(EmbedFieldBuilder::new("Roles", roles).inline())
+            .build();
 
         ctx.http
             .create_message(req.interaction.channel_id.unwrap())
