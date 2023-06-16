853e6566b945ca44378ff636d589682fb82d70b1
Samzyre <samihtk@outlook.com>
2023-06-16 17:41:56 +0300

    Primitive music player

--- src/commands/bot/meta/voice.rs
+++ src/commands/bot/meta/voice.rs
@@ -1,3 +1,5 @@
+use songbird::input::{Input, YoutubeDl};
+use songbird::tracks::Track;
 use twilight_model::channel::ChannelType;
 use twilight_model::id::marker::{ChannelMarker, GuildMarker, UserMarker};
 use twilight_model::id::Id;
@@ -28,6 +30,17 @@ impl Voice {
                 sub("leave", "Disconnect the bot from a voice channel.")
                     .attach(Leave::classic)
                     .attach(Leave::slash),
+            )
+            .option(
+                sub("play", "Play a sound or music on voice (queued).")
+                    .attach(Play::classic)
+                    .attach(Play::slash)
+                    .option(string("url", "Youtube URL to play.").required()),
+            )
+            .option(
+                sub("skip", "Go to the next track in queue.")
+                    .attach(Skip::classic)
+                    .attach(Skip::slash),
             )
     }
 
@@ -77,8 +90,10 @@ impl Join {
             .join(guild_id, channel_id)
             .await
             .with_context(|| format!("Failed to join channel '{channel_id}'"))
-            .map(|_| {
+            .map(|c| {
                 Ok(Response::new(move || async move {
+                    let deaf = c.lock().await.deafen(true).await;
+                    deaf.context("Failed to deafen")?;
                     info!("Connected to voice channel '{channel_id}'");
                     Ok(())
                 }))
@@ -128,7 +143,7 @@ impl Leave {
         };
 
         ctx.voice
-            .leave(guild_id)
+            .remove(guild_id)
             .await
             .with_context(|| format!("Failed to leave channel '{channel_id}'"))
             .map(|_| {
@@ -145,5 +160,124 @@ impl Leave {
 
     async fn slash(ctx: Context, req: SlashRequest) -> CommandResponse {
         Self::uber(ctx, req.interaction.guild_id).await
+    }
+}
+
+/// Command: Play a sound or music in voice.
+struct Play;
+
+impl Play {
+    async fn uber(
+        ctx: Context,
+        args: Args,
+        guild_id: Id<GuildMarker>,
+        channel_id: Id<ChannelMarker>,
+    ) -> CommandResponse {
+        let Some(call) = ctx.voice.get(guild_id) else {
+            info!("No voice connection found for '{guild_id}'");
+            return Ok(Response::none());
+        };
+
+        let url = args.string("url")?;
+        let client = reqwest::Client::new();
+        let mut input = Input::from(YoutubeDl::new(client, url.into_string()));
+        let meta = input.aux_metadata().await;
+        let track = Track::new(input).volume(0.5);
+        let handle = call.lock().await.enqueue(track).await;
+        let result = handle
+            .make_playable_async()
+            .await
+            .with_context(|| format!("Cannot play audio track"));
+
+        match meta {
+            Ok(metadata) => {
+                let content = format!(
+                    "Playing **{}** by **{}**",
+                    metadata
+                        .title
+                        .or(metadata.track)
+                        .unwrap_or_else(|| "<UNKNOWN>".to_string()),
+                    metadata.artist.unwrap_or_else(|| "<UNKNOWN>".to_string()),
+                );
+
+                ctx.http
+                    .create_message(channel_id)
+                    .content(&content)?
+                    .await?;
+
+                // Metadata was ok, but playing failed?
+                result?;
+            },
+            Err(e) => {
+                eprintln!("Metadata error: {e}");
+                info!("Metadata error: {e}");
+
+                if let Err(r) = result {
+                    eprintln!("Cannot play audio track: {r}");
+                    info!("Cannot play audio track: {r}");
+
+                    ctx.http
+                        .create_message(channel_id)
+                        .content(&format!("I can't play that :mute:"))?
+                        .await?;
+                } else {
+                    info!("Something without metadata is playing");
+                }
+            },
+        }
+
+        Ok(Response::none())
+    }
+
+    async fn classic(ctx: Context, req: ClassicRequest) -> CommandResponse {
+        Self::uber(
+            ctx,
+            req.args,
+            req.message.guild_id.ok_or(CommandError::Disabled)?,
+            req.message.channel_id,
+        )
+        .await
+    }
+
+    async fn slash(ctx: Context, req: SlashRequest) -> CommandResponse {
+        Self::uber(
+            ctx,
+            req.args,
+            req.interaction.guild_id.ok_or(CommandError::Disabled)?,
+            req.interaction
+                .channel
+                .as_ref()
+                .map(|c| c.id)
+                .context("No channel found")?,
+        )
+        .await
+    }
+}
+
+/// Command: Go to the next track in queue.
+struct Skip;
+
+impl Skip {
+    async fn uber(ctx: Context, guild_id: Id<GuildMarker>) -> CommandResponse {
+        let Some(call) = ctx.voice.get(guild_id) else {
+            info!("No voice connection found for '{guild_id}'");
+            return Ok(Response::none());
+        };
+
+        call.lock()
+            .await
+            .queue()
+            .skip()
+            .context("Failed to skip audio track")?;
+
+        Ok(Response::none())
+    }
+
+    async fn classic(ctx: Context, req: ClassicRequest) -> CommandResponse {
+        Self::uber(ctx, req.message.guild_id.ok_or(CommandError::Disabled)?).await
+    }
+
+    async fn slash(ctx: Context, req: SlashRequest) -> CommandResponse {
+        Self::uber(ctx, req.interaction.guild_id.ok_or(CommandError::Disabled)?).await
     }
 }
