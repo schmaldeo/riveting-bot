ff6faa59a76bd832e58321db1cad5039ee50d994
schmatteo <michal.skrzyszowski@hotmail.com>
2023-11-03 23:19:53 +0100

    Send messages asking to place ships in battleships

--- src/commands/bot/user/battleships.rs
+++ src/commands/bot/user/battleships.rs
@@ -1,6 +1,7 @@
 use pyo3::prelude::*;
 use crate::utils::prelude::*;
 use twilight_model::http::permission_overwrite::{PermissionOverwrite, PermissionOverwriteType};
+use twilight_model::gateway::payload::incoming::MessageCreate;
 use twilight_model::guild::Permissions;
 use twilight_model::channel::Channel;
 use twilight_util::builder::embed::{self, EmbedFieldBuilder};
@@ -25,6 +26,7 @@ impl Battleships {
         let players = [player1, player2];
         let guild_id = req.interaction.guild_id.unwrap();
         let mut channels: Vec<Channel> = Vec::new();
+        let ship_types = ["destroyer", "cruiser", "battleship"];
 
         let everyone_permission_overwrite = PermissionOverwrite { allow: (None), deny: (Some(Permissions::VIEW_CHANNEL)), id: (guild_id.cast()), kind: (PermissionOverwriteType::Role) };
 
@@ -36,7 +38,7 @@ impl Battleships {
             Ok(game.unwrap().to_object(py))
         });
 
-        for (index, player) in players.into_iter().enumerate() {
+        for (index, player) in players.iter().enumerate() {
           let channel = ctx.http.create_guild_channel(guild_id, &format!("Player {}", index + 1)).unwrap().send().await?;
           let player_permission_overwrite = PermissionOverwrite { allow: (Some(Permissions::VIEW_CHANNEL)), deny: (None), id: (player.cast()), kind: (PermissionOverwriteType::Member) };
           ctx.http.update_channel_permission(channel.id, &everyone_permission_overwrite).await?;
@@ -45,19 +47,40 @@ impl Battleships {
         }
 
         for (index, channel) in channels.iter().enumerate() {
+            // Get a board for the player and create an embed for it
             let board = Python::with_gil(|py| -> PyResult<String> {
                 let board = game.as_ref().unwrap().getattr(py, format!("player{}", index + 1).as_str()).unwrap().call_method0(py, "get_stringified_board").unwrap();
     
                 board.extract::<String>(py)
             });
-
             let embed = embed::EmbedBuilder::new()
                 .title("Battleships")
                 .color(0x9500a8)
                 .field(EmbedFieldBuilder::new("Board", format!("Your board:\n```{}```", board.unwrap())))
                 .build();
-
             ctx.http.create_message(channel.id).embeds(&[embed]).unwrap().await?;
+
+            // TODO both players should be able to place ships at the same time
+            // While the first player places their ships, the second one gets a message asking them to wait
+            if index == 0 {
+                let wait_embed = embed::EmbedBuilder::new().title("Please wait while the other player places his ships").build();
+                ctx.http.create_message(channels[index + 1].id).embeds(&[wait_embed]).unwrap().await?;
+            }
+            
+            for ship_type in ship_types {
+                let place_embed = embed::EmbedBuilder::new()
+                    .title(format!("Select a place for your **{}**", ship_type))
+                    .field(EmbedFieldBuilder::new("Message format: `x, y, orientation [v/h]`", "e.g. 3, 2, h"))
+                    .build();
+                ctx.http.create_message(channel.id).embeds(&[place_embed]).unwrap().await?;
+  
+                let message = ctx.standby
+                    .wait_for_message(channel.id, move |event: &MessageCreate| {
+                        event.content.split(',').count() == 3
+                    })
+                    .await?;
+                println!("{}", message.author.name);
+            }
         }
 
         Ok(Response::clear(ctx, req))
